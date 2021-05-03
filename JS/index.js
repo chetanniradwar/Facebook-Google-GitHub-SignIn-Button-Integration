9ae23d1a2c78a5a0c258618398af1781dc655d85
chetannirdwar <chetanniradwar@gmail.com>
2021-05-03 18:30:51 +0530

    UPDATED FOR FACEBOOK LOGIN CHECK

--- JS/index.js
+++ JS/index.js
@@ -20,6 +20,23 @@ localStorage.setItem("email",email);
 }
 
   // Facebook login
+
+  function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+    console.log('statusChangeCallback');
+    console.log(response);                   // The current login status of the person.
+    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+      testAPI();  
+    } else {                                 // Not logged into your webpage or we are unable to tell.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this webpage.';
+    }
+  }
+  
+  function checkLoginState() {               // Called when a person is finished with the Login Button.
+    FB.getLoginStatus(function(response) {   // See the onlogin handler
+      statusChangeCallback(response);
+    });
+  }
   window.fbAsyncInit = function() {
     FB.init({
       appId      : '367986131263200',
@@ -28,20 +45,25 @@ localStorage.setItem("email",email);
       version    : '10.0'
     });
       
-    FB.AppEvents.logPageView();   
-      
-  };
+    FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
+      statusChangeCallback(response);        // Returns the login status.
+    });
+    };
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+  
+
+  function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
+    });
+  }
 
 
-
+  
+   
   //  Video playback Speed
  
  
@@ -52,3 +74,26 @@ function PlayBack(){
   vdo.playbackRate=0.5;
   // alert(vdo.playbackRate);
   }
+
+
+  
+
+  // window.fbAsyncInit = function() {
+  //   FB.init({
+  //     appId      : '{your-app-id}',
+  //     cookie     : true,
+  //     xfbml      : true,
+  //     version    : '{api-version}'
+  //   });
+      
+  //   FB.AppEvents.logPageView();   
+      
+  // };
+
+  // (function(d, s, id){
+  //    var js, fjs = d.getElementsByTagName(s)[0];
+  //    if (d.getElementById(id)) {return;}
+  //    js = d.createElement(s); js.id = id;
+  //    js.src = "https://connect.facebook.net/en_US/sdk.js";
+  //    fjs.parentNode.insertBefore(js, fjs);
+  //  }(document, 'script', 'facebook-jssdk'));
