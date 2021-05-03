83079e6056cd4ff2c7d9ceece44e86919c0e127f
chetannirdwar <chetanniradwar@gmail.com>
2021-05-03 23:58:18 +0530

    fb login test

--- JS/index.js
+++ JS/index.js
@@ -23,26 +23,26 @@ function onSignIn(googleUser) {
 // Facebook login
 
 
-function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
-  console.log('statusChangeCallback');
-  console.log(response);                   // The current login status of the person.
-  if (response.status === 'connected') {   // Logged into your webpage and Facebook.
-    // testAPI(); 
-    fb_login();
-    // FB.login();
+// function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+//   console.log('statusChangeCallback');
+//   console.log(response);                   // The current login status of the person.
+//   if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+//     // testAPI(); 
+//     fb_login();
+//     // FB.login();
 
 
-  } else {                                 // Not logged into your webpage or we are unable to tell.
-    document.getElementById('status').innerHTML = 'Please log ' +
-      'into this webpage.';
-  }
-}
+//   } else {                                 // Not logged into your webpage or we are unable to tell.
+//     document.getElementById('status').innerHTML = 'Please log ' +
+//       'into this webpage.';
+//   }
+// }
 
-function checkLoginState() {               // Called when a person is finished with the Login Button.
-  FB.getLoginStatus(function (response) {   // See the onlogin handler
-    statusChangeCallback(response);
-  });
-}
+// function checkLoginState() {               // Called when a person is finished with the Login Button.
+//   FB.getLoginStatus(function (response) {   // See the onlogin handler
+//     statusChangeCallback(response);
+//   });
+// }
 window.fbAsyncInit = function () {
   FB.init({
     appId: '367986131263200',
@@ -73,14 +73,14 @@ function fb_login() {
   }, { scope: 'name,email,public_profile' });
 
 }
-function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
-  console.log('Welcome!  Fetching your information.... ');
-  FB.api('/me', function (response) {
-    console.log('Successful login for: ' + response.name);
-    document.getElementById('status').innerHTML =
-      'Thanks for logging in, ' + response.name + '!' + response.public_profile + response.email;
-  });
-}
+// function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+//   console.log('Welcome!  Fetching your information.... ');
+//   FB.api('/me', function (response) {
+//     console.log('Successful login for: ' + response.name);
+//     document.getElementById('status').innerHTML =
+//       'Thanks for logging in, ' + response.name + '!' + response.public_profile + response.email;
+//   });
+// }
 
 
 
